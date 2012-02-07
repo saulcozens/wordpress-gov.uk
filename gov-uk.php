5002438b11a782b320584bbf74544d424556a06c
Simon Dickson <simon@puffbox.com>
2012-02-07 19:24:00 +0000

    Removes trailing slashes on URLs, wraps output in a div for easier targeting

--- gov-uk.php
+++ gov-uk.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gov-UK
 Plugin URI: http://saulcozens.co.uk/wordpress/gov.uk
-Description: provides a shortcode for inlcuding gov.uk content into a page.  
+Description: provides a shortcode for including gov.uk content into a page.
 Version: 0.1alpha
 Author: Saul Cozens
 Author URI: http://saulcozens.co.uk/
@@ -15,12 +15,14 @@ Author URI: http://saulcozens.co.uk/
 class govuk {
 	function insert_content($attr) {
 		extract(shortcode_atts(array(
-			'url' => 'https://gov.uk/'
-			), $attr));
+					'url' => 'https://gov.uk/'
+				), $attr));
 
 		$ch = curl_init();
-			
-		$url=parse_url($url, PHP_URL_PATH);
+
+		$url = untrailingslashit($url); // in case url has a slash on the end
+
+		$url = parse_url($url, PHP_URL_PATH);
 
 		if(isset($_GET['govuk']) && strpos($url,rawurldecode($_GET['govuk']))==0) { // do we have a url in the query string and does it start withe the attribute of the shortcode?
 			$url="https://www.gov.uk".rawurldecode($_GET['govuk']);
@@ -42,9 +44,9 @@ class govuk {
 		} else {
 			$url="https://www.gov.uk".$url;
 		}
-		
+
 		$url=str_replace(parse_url($url, PHP_URL_PATH),parse_url($url, PHP_URL_PATH).".json",$url); //fancy way of sticking .json on the url while maintaining the query string.  this is going to have issues with any path that exists in the domain.  ####BAD CODE
-		
+
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -54,7 +56,9 @@ class govuk {
 
 		$govuk_json=json_decode($response);
 		require_once('simplehtmldom/simple_html_dom.php');
-		$govuk_html_fragment=str_get_html($govuk_json->html_fragment);
+
+		$govuk_html_fragment = str_get_html($govuk_json->html_fragment);
+
 		foreach($govuk_html_fragment->find("[href]") as $e) {
 			if((parse_url($e->href,PHP_URL_HOST)=='') || (parse_url($e->href,PHP_URL_HOST)=='www.gov.uk')) {
 				$e->href= add_query_arg("govuk",urlencode($e->href),get_permalink());
@@ -66,7 +70,12 @@ class govuk {
 			}
 		}
 
-		return $govuk_html_fragment;
+		// wraps output in a .govuk class, for easier CSS targeting
+		$output = '<div class="govuk">';
+		$output .= $govuk_html_fragment;
+		$output .= '</div><!-- end .govuk -->';
+
+		return $output;
 	}
 
 }
